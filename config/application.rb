579e045ce683f4083e47d34319d7d84d498e2395
DX <ivansizyh26@gmail.com>
2021-12-05 23:11:38 +0700

    New fix commit

--- config/application.rb
+++ config/application.rb
@@ -7,11 +7,11 @@ require "rails/all"
 Bundler.require(*Rails.groups)
 
 module TestGuru
+  VERSION = "0.1.0"
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
     config.time_zone = "Krasnoyarsk"
-    config.active_record.default_timezone = :utc
 
     config.i18n.default_locale = :ru
     # Configuration for the application, engines, and railties goes here.
