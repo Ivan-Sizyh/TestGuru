6f7aabd8a0b5335c247060d97d7622eb5eb49083
DX <ivansizyh26@gmail.com>
2021-12-09 22:01:03 +0700

    Lesson 3 commit

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,6 @@
 class Test < ApplicationRecord
+  def self.give_tests_by_category(category)
+    category_id = Category.where(title: category).pluck(:id)
+    self.class.order(id: :desc).where(category: category_id).pluck(:title)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  def get_tests_by_level(level)
+    tests = Result.where(user: self.id).pluck(:test_id)
+    Test.where(id: tests, level: level)
+  end
 end
