651503495a6d27a8ac786dade3411e8bcc42c46b
DX <ivansizyh26@gmail.com>
2021-12-09 22:50:57 +0700

    Fix commit

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,5 @@
 class Test < ApplicationRecord
   def self.give_tests_by_category(category)
-    category_id = Category.where(title: category).pluck(:id)
-    self.class.order(id: :desc).where(category: category_id).pluck(:title)
+    Test.joins(:categories).where(title: category).order(title: :desc).pluck(:title)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
   def get_tests_by_level(level)
-    tests = Result.where(user: self.id).pluck(:test_id)
-    Test.where(id: tests, level: level)
+    Test.joins(:results).where(level: level)
   end
 end
