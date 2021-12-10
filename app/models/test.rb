d093c43b99508ef30d4ce6094ed015f98a895239
DX <ivansizyh26@gmail.com>
2021-12-10 19:01:47 +0700

    Use SQL rows in model methods

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,5 +1,6 @@
 class Test < ApplicationRecord
   def self.give_tests_by_category(category)
-    Test.joins(:categories).where(title: category).order(title: :desc).pluck(:title)
+    Test.joins("JOIN categories ON tests.category_id = categories.id AND categories.title = ?", category)
+        .record(title: :desc).pluck(:title)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   def get_tests_by_level(level)
-    Test.joins(:results).where(level: level)
+    Test.joins("JOIN results ON tests.id = results.test_id AND results.user_id = ?", id).where(level: level)
   end
 end
