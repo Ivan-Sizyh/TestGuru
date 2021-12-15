7ed38dfae89829487f6ffb18bc7719671a103929
Ivan Sizyh <ivansizyh26@gmail.com>
2021-12-15 16:19:59 +0700

    Change method arguments

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,7 +1,7 @@
 class Test < ApplicationRecord
-  def self.give_tests_by_category(category)
+  def self.give_tests_by_category(category_title)
     Test.joins("JOIN categories ON tests.category_id = categories.id")
-        .where(categories: {title: category})
+        .where(categories: {title: category_title})
         .order(title: :desc)
         .pluck(:title)
   end
