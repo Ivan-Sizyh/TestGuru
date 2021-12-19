3605d09c490a8bc8f4f1874a7ad7fec205804a7e
Ivan Sizyh <ivansizyh26@gmail.com>
2021-12-20 04:34:31 +0700

    Lesson 5 add scope methods, validations

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,3 +1,15 @@
 class Answer < ApplicationRecord
   belongs_to :question
+
+  validates :text, presence: true
+  validate :validate_limit_answer, on: :create, if: -> { question.present? }
+
+  scope :correct, -> { where(correct: true) }
+
+  private
+
+  def validate_limit_answer
+    errors.add(:base, :limit_answers, message: 'The question has the maximum ' +
+      'number of answers') if question.answers.size > 4
+  end
 end
