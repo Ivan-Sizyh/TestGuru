3ab6ecbf4304f441f5db2dfe55964f9250d852c6
Ivan Sizyh <ivansizyh26@gmail.com>
2021-12-22 16:05:29 +0700

    Add callback. Change new.html.

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,6 @@
 class QuestionsController < ApplicationController
   before_action :find_test, only: %i[index create new]
+  before_action :find_question, only: %i[show destroy edit]
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
@@ -10,7 +11,7 @@ class QuestionsController < ApplicationController
   end
 
   def show
-    render plain: Question.find(params[:id]).inspect
+    render plain: @question.inspect
   end
 
   def new
@@ -18,11 +19,12 @@ class QuestionsController < ApplicationController
 
   def create
     question = @test.questions.build(question_params)
+
     render plain: question.inspect if question.save
   end
 
   def destroy
-    render plain: 'Тест успешно удален' if Question.destroy(params[:id])
+    render plain: 'Тест успешно удален' if @question.destroy
   end
 
   private
@@ -37,5 +39,9 @@ class QuestionsController < ApplicationController
 
   def rescue_with_question_not_found
     head 404
+  end
+
+  def find_question
+    @question = Question.find(params[:id])
   end
 end
