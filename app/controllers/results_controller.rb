faa4bc011e0e7879a8146722459f2b32ad77274a
Ivan Sizyh <ivansizyh26@gmail.com>
2022-01-25 00:04:42 +0700

    Lesson 11 fix commit

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,6 +1,6 @@
 class ResultsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_result, only: %i[show update score gist]
+  before_action :set_result, only: %i[show update score]
 
   def show
   end
--- app/services/gist_question_service.rb
+++ app/services/gist_question_service.rb
@@ -1,15 +1,17 @@
 class GistQuestionService
+  GITHUB_ACCESS_TOKEN = Rails.application.credentials.github[:github_access_token]
+
   Result = Struct.new(:url, :success?)
 
-  def initialize(question, client: nil)
+  def initialize(question, client: default_client)
     @question = question
     @test = question.test
-    @client = client || GitHubClient.new
+    @client = client
   end
 
   def call
-    gist_info = @client.create_gists(gist_params)
-    Result.new(gist_info.url, @client.get_gist(gist_info.id).any?)
+    gist_info = @client.create_gist(gist_params)
+    Result.new(gist_info.url, @client.last_response.status == 201)
   end
 
   private
@@ -27,5 +29,9 @@ class GistQuestionService
 
   def gist_content
     [@question.body, *@question.answers.pluck(:text)].join("\n")
+  end
+
+  def default_client
+    Octokit::Client.new(access_token: GITHUB_ACCESS_TOKEN)
   end
 end
