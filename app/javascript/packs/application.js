b907aa56b4e7bfd8009be7597005b79fd7cfeff9
Ivan Sizyh <ivansizyh26@gmail.com>
2022-01-26 19:19:41 +0700

    Lesson 12 fix commit

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,9 @@ import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "utilities/sorting"
 import "utilities/password_confirmation"
+import "utilities/form_inline"
+import "utilities/progress_bar"
+
 
 Rails.start()
 Turbolinks.start()
